685c270733c83c25b15a7e3244f55e2dcfde3f19
bharadwaja-rao-d <cs20btech11012@iith.ac.in>
2023-07-10 17:24:38 +0530

    added .gitignore

--- render-app/src/components/renderer/Renderer.tsx
+++
@@ -1,9 +0,0 @@
-type rendererProps = {
-    drawing: Drawing
-}
-
-export default function Renderer({drawing}: rendererProps) {
-    return (
-            <div className="renderer"></div>
-           );
-}
